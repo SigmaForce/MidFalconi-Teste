ec6fbb8324665f9482f03932a021b14b9272efc5
Leonardo Matos <leonardolucas2020@gmail.com>
2025-11-11 15:18:49 -0400

    :sparkles: feat: integrate `user-form` with `api`

--- web/src/app/(base)/register/_components/user-form.tsx
+++ web/src/app/(base)/register/_components/user-form.tsx
@@ -9,43 +9,71 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { type UserFormValues, userSchema } from "@/schemas/user-schema";
+import { useProfiles } from "@/hooks/profile/useProfiles";
+import { useCreateUser } from "@/hooks/users/useCreateUser";
+import { Profile } from "@/http/types/profile.type";
 
+import { type UserFormValues, userSchema } from "@/schemas/user-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 export function UserForm() {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
     formState: { errors },
     setValue,
+    reset,
   } = useForm<UserFormValues>({
     resolver: zodResolver(userSchema),
     defaultValues: {
-      nome: "",
-      sobrenome: "",
+      firstName: "",
+      lastName: "",
       email: "",
-      perfil: "",
+      profileId: "",
     },
   });
 
-  const onSubmit = (values: UserFormValues) => {
-    console.log(values);
+  const { data: profiles, isLoading: isLoadingProfiles } = useProfiles();
+  const createUser = useCreateUser();
+
+  const onSubmit = async (values: UserFormValues) => {
+    try {
+      const user = await createUser.mutateAsync(values);
+
+      toast.success("Usuário criado com sucesso!", {
+        description: `ID: ${user.id}`,
+        action: {
+          label: "Copiar ID",
+          onClick: () => {
+            navigator.clipboard.writeText(user.id);
+            toast.info("ID copiado para a área de transferência!");
+            router.push(`/`);
+          },
+        },
+      });
+
+      reset();
+    } catch (error: any) {
+      toast.error("Erro ao criar o usuário");
+    }
   };
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="space-y-2 w-full max-w-sm"
+      className="space-y-3 w-full max-w-sm"
     >
       <div>
         <label className="block text-sm font-medium text-zinc-700 mb-1">
           Nome
         </label>
-        <Input placeholder="Nome" {...register("nome")} />
-        {errors.nome && (
-          <p className="text-sm text-red-500">{errors.nome.message}</p>
+        <Input placeholder="Nome" {...register("firstName")} />
+        {errors.firstName && (
+          <p className="text-sm text-red-500">{errors.firstName.message}</p>
         )}
       </div>
 
@@ -53,9 +81,9 @@ export function UserForm() {
         <label className="block text-sm font-medium text-zinc-700 mb-1">
           Sobrenome
         </label>
-        <Input placeholder="Sobrenome" {...register("sobrenome")} />
-        {errors.sobrenome && (
-          <p className="text-sm text-red-500">{errors.sobrenome.message}</p>
+        <Input placeholder="Sobrenome" {...register("lastName")} />
+        {errors.lastName && (
+          <p className="text-sm text-red-500">{errors.lastName.message}</p>
         )}
       </div>
 
@@ -77,23 +105,39 @@ export function UserForm() {
         <label className="block text-sm font-medium text-zinc-700 mb-1">
           Perfil
         </label>
-        <Select onValueChange={(value) => setValue("perfil", value)}>
+        <Select
+          onValueChange={(value) => setValue("profileId", value)}
+          disabled={isLoadingProfiles}
+        >
           <SelectTrigger className="w-full">
-            <SelectValue placeholder="Selecione um perfil" />
+            <SelectValue
+              placeholder={
+                isLoadingProfiles
+                  ? "Carregando perfis..."
+                  : "Selecione um perfil"
+              }
+            />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="admin">Administrador</SelectItem>
-            <SelectItem value="user">Usuário</SelectItem>
-            <SelectItem value="guest">Convidado</SelectItem>
+            {profiles?.map((profile: Profile) => (
+              <SelectItem key={profile.id} value={profile.id}>
+                {profile.name}
+              </SelectItem>
+            ))}
           </SelectContent>
         </Select>
-        {errors.perfil && (
-          <p className="text-sm text-red-500">{errors.perfil.message}</p>
+        {errors.profileId && (
+          <p className="text-sm text-red-500">{errors.profileId.message}</p>
         )}
       </div>
 
-      <Button type="submit" variant="default" className="w-full">
-        Salvar Usuário
+      <Button
+        type="submit"
+        variant="default"
+        className="w-full"
+        disabled={createUser.isPending}
+      >
+        {createUser.isPending ? "Salvando..." : "Salvar Usuário"}
       </Button>
     </form>
   );
--- web/src/schemas/user-schema.ts
+++ web/src/schemas/user-schema.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-  nome: z.string().min(2, "O nome deve ter pelo menos 2 caracteres"),
-  sobrenome: z.string().min(2, "O sobrenome deve ter pelo menos 2 caracteres"),
+  firstName: z.string().min(2, "O nome deve ter pelo menos 2 caracteres"),
+  lastName: z.string().min(2, "O sobrenome deve ter pelo menos 2 caracteres"),
   email: z.email("Informe um e-mail válido"),
-  perfil: z.string().min(1, "Selecione um perfil"),
+  profileId: z.uuid().min(1, "Selecione um perfil"),
 });
 
 export type UserFormValues = z.infer<typeof userSchema>;
